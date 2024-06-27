ee053c1ba0d9afcda337d468f3e1800929c61d97
Moe Yan Htun <moeyan227970@gmail.com>
2024-06-27 21:17:52 +0630

    [Complete] Delete Button With Custom CSS

--- DotNet7.BlazorWebApp/Pages/Blog/BlogTable.razor.cs
+++ DotNet7.BlazorWebApp/Pages/Blog/BlogTable.razor.cs
@@ -1,8 +1,10 @@
-﻿namespace DotNet7.BlazorWebApp.Pages.Blog
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace DotNet7.BlazorWebApp.Pages.Blog
 {
     public partial class BlogTable
     {
-        string[] headings = { "ID", "Name", "Email", "Gender", "IP Address" };
         string[] rows =
         {
             @"1 Krishna kpartner0@usatoday.com Male 28.25.250.202",
@@ -12,5 +14,25 @@
             @"5 Cecilius cchaplin4@shinystat.com Male 195.124.144.18",
             @"6 Cicely cemerine9@soup.io Female 138.94.191.43",
         };
+
+        string state = "Message box hasn't been opened yet";
+
+
+        private async void OnButtonClicked()
+        {
+            var options = new DialogOptions
+            {
+                CloseOnEscapeKey = true,
+                FullWidth = true,
+                MaxWidth = MaxWidth.ExtraSmall,
+                ClassBackground = "my-custom-class"
+            };
+            bool? result = await DialogService.ShowMessageBox(
+                "Warning",
+                "Deleting can not be undone!",
+                yesText: "Delete!", cancelText: "Cancel", options: options);
+            state = result == null ? "Canceled" : "Deleted!";
+            StateHasChanged();
+        }
     }
 }
